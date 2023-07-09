b4928ff8cd3a305b169ea9be94a37d3376797ae7
AleksNau <workciti@yandex.ru>
2023-07-09 15:07:31 +0300

    сделать рефакторинг

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CurrentUserContext from "../contexts/CurrentUserContext";
 import Card from "./Card";
+import SignUp from "./Sign-up";
 
 const Main = ({
   cards,
@@ -61,54 +62,7 @@ const Main = ({
           />
         ))}
       </ul>
-      <div className="sign-up">
-          <form onSubmit={(event) => {
-              event.preventDefault();
-          }} name={`sign-up-form`} className={`popup__form popup__form_sign-up`} id={`sign-up-form`}
-                method="post"
-          >
-
-              <fieldset className="sign-up__fieldset">
-                  <legend className="sign-up__title">Регистрация</legend>
-
-                  <label>
-                      <input
-                          name="email"
-                          id="email"
-                          type="email"
-                          className="sign-up__input"
-                          minLength="2"
-                          maxLength="40"
-                          placeholder="Email"
-                          required
-                      />
-                      <span id="name-error" className="popup__error">
-              &nbsp;
-            </span>
-                  </label>
-                  <label>
-                      <input
-                          name="password"
-                          id="pass"
-                          type="text"
-                          className="sign-up__input"
-                          minLength="2"
-                          maxLength="30"
-                          placeholder="Пароль"
-                          required
-                      />
-                      <span id="info-error" className="popup__error">
-              &nbsp;
-            </span>
-                  </label>
-                  <button type="submit" className="sign-up__submit" form={`sign-up-form`}
-                          value="Зарегистрироваться">Зарегистрироваться</button>
-                  <p>Уже зарегистрированы? Войти</p>
-              </fieldset>
-
-          </form>
-
-      </div>
+      <SignUp></SignUp>
     </main>
   );
 };
