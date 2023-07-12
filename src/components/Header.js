3f40c4d23b5476d857276cd3bc4b277acc420bfd
AleksNau <workciti@yandex.ru>
2023-07-12 15:20:22 +0300

    сделать отступы и кнопку в хэдере

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router";
 import logo from "../images/logo.svg";
 
-const Header = () => {
+const Header = ({loggedIn}) => {
   return (
     <header className="header">
       <img src={logo} alt="логотип" className="header__logo" />
+      <Routes>
+        <Route path="/sign-in" element={<button className="header__button">Зарегистрироваться</button>}></Route>
+        <Route path="/sign-up" element={<button className="header__button">Войти</button>}></Route>
+        <Route path="/" element={<button className="header__button">Email Выйти</button>}></Route>
+      </Routes>
+      
     </header>
   );
 };
