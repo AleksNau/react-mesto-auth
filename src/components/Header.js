803b855856b9450d0980e0c3270d9fc5e9911cba
AleksNau <workciti@yandex.ru>
2023-07-12 15:31:36 +0300

    сделать хэддер

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { Route, Routes } from "react-router";
+import { Route, Routes,useNavigate } from "react-router-dom";
 import logo from "../images/logo.svg";
 
-const Header = ({loggedIn}) => {
+const Header = () => {
+  const navigate = useNavigate();
   return (
     <header className="header">
       <img src={logo} alt="логотип" className="header__logo" />
       <Routes>
-        <Route path="/sign-in" element={<button className="header__button">Зарегистрироваться</button>}></Route>
-        <Route path="/sign-up" element={<button className="header__button">Войти</button>}></Route>
-        <Route path="/" element={<button className="header__button">Email Выйти</button>}></Route>
+        <Route path="/sign-in" element={<button className="header__button" onClick={() => {navigate("/sign-up")}}>Зарегистрироваться</button>}></Route>
+        <Route path="/sign-up" element={<button className="header__button" onClick={() => {navigate("/sign-in")}}>Войти</button>}></Route>
+        <Route path="/" element={<button className="header__button" onClick={() => {navigate("/sign-in")}}>Email Выйти</button>}></Route>
       </Routes>
       
     </header>
