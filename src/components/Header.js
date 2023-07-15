ec2bbf7820b7a57a9651efc5538cadfe5087670c
AleksNau <workciti@yandex.ru>
2023-07-15 12:08:10 +0300

    сделать хэддер эмаил

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,14 @@ const Header = ({email,signOut}) => {
       <Routes>
         <Route path="/sign-in" element={<button className="header__button" onClick={() => {navigate("/sign-up")}}>Регистрация</button>}></Route>
         <Route path="/sign-up" element={<button className="header__button" onClick={() => {navigate("/sign-in")}}>Войти</button>}></Route>
-        <Route path="/" element={<button className="header__button" onClick={signOut}>{`${email} Выйти`}</button>}></Route>
+        <Route path="/" element={<div className="header__container">
+            <span className="header__email">{email}</span>
+            <button className="header__button" onClick={signOut}>
+                Выйти
+            </button>
+        </div>}></Route>
       </Routes>
-      
+
     </header>
   );
 };
